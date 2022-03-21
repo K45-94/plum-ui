675088c34c26489a30f772891ea5d10ecee7049e
K45-94 <kelvinhiuhu@live.com>
2022-03-22 01:43:19 +0300

    message

--- src/pages/PageSearch.vue
+++ src/pages/PageSearch.vue
@@ -3,9 +3,11 @@
 </template>
 
 <script>
+import Page from "src/components/PagePlumComponent/Page.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  components: { Page },
   name: "PageSearch",
 });
 </script>
