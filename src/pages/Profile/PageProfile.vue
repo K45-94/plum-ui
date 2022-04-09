1a9017f95d627217a0ee690d33f8a7203e04ad75
K45-94 <kelvinhiuhu@live.com>
2022-04-09 06:44:41 +0300

    message

--- src/pages/Profile/PageProfile.vue
+++ src/pages/Profile/PageProfile.vue
@@ -17,7 +17,7 @@
 import { defineComponent } from "vue";
 import Page from "src/components/PagePlumComponent/Page.vue";
 import PageHeader from "src/components/PagePlumComponent/PageHeader.vue";
-import PageHeaderButtonBackRight from "src/components/PagePlumComponent/PageHeaderButtonForwardRight.vuevue";
+import PageHeaderButtonBackRight from "src/components/PagePlumComponent/PageHeaderButtonForwardRight.vue";
 import PageBody from "src/components/PagePlumComponent/PageBody.vue";
 
 export default defineComponent({
