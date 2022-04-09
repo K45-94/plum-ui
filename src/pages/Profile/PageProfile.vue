bfa4719ad6d58049004e815a9a9221d1d22e096b
K45-94 <kelvinhiuhu@live.com>
2022-04-09 06:35:20 +0300

    Revert "message"
    This reverts commit 3abf45a9691dffc38b2650246e0d7c65605ee2bd.

--- src/pages/Profile/PageProfile.vue
+++ src/pages/Profile/PageProfile.vue
@@ -3,7 +3,7 @@
     <page-header>
       <template #title> PROFILE </template>
       <template #button-right>
-        <page-header-button-forward-right
+        <page-header-button-back-right
           to="/profile/settings"
           label="settings"
         />
@@ -17,11 +17,11 @@
 import { defineComponent } from "vue";
 import Page from "src/components/PagePlumComponent/Page.vue";
 import PageHeader from "src/components/PagePlumComponent/PageHeader.vue";
-import PageHeaderButtonForwardRight from "src/components/PagePlumComponent/PageHeaderButtonForwardRight.vue";
+import PageHeaderButtonBackRight from "src/components/PagePlumComponent/PageHeaderButtonForwardRight.vuevue";
 import PageBody from "src/components/PagePlumComponent/PageBody.vue";
 
 export default defineComponent({
-  components: { Page, PageHeader, PageBody, PageHeaderButtonForwardRight },
+  components: { Page, PageHeader, PageBody, PageHeaderButtonBackRight },
   name: "PageProfile",
 });
 </script>
