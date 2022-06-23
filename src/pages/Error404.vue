fe7246a67e5d31839077a555e42395f5ce873f92
K45-94 <kelvinhiuhu@live.com>
2022-06-23 15:51:03 +0300

    message

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -5,7 +5,7 @@
     <div>
       <div style="font-size: 30vh">404</div>
 
-      <div class="text-h2" style="opacity: 0.4">Oops. No Plums here...</div>
+      <div class="text-h4" style="opacity: 0.4">Oops. No Plums here...</div>
 
       <q-btn
         class="q-mt-xl"
